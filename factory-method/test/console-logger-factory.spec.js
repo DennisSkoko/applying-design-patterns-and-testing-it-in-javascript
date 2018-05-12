a1d32c45ca475969102c644cb461340f11238e86
Dennis Skoko <dennis.skoko@hotmail.com>
2018-05-12 18:45:52 +0200

    Moved the tests into the nested describe

--- factory-method/test/console-logger-factory.spec.js
+++ factory-method/test/console-logger-factory.spec.js
@@ -7,22 +7,22 @@ const proxyquire = require('proxyquire')
 const Logger = require('../src/logger')
 
 describe('Factory Method', function () {
-  let ConsoleLoggerFactory = null
-  let loggerSpy = null
-
-  before('require the module', function () {
-    loggerSpy = sinon.spy(Logger)
-
-    ConsoleLoggerFactory = proxyquire('../src/console-logger-factory', {
-      './logger': loggerSpy
-    })
-  })
-
-  afterEach('reset the history', function () {
-    loggerSpy.resetHistory()
-  })
-
   describe('console-logger-factory', function () {
+    let ConsoleLoggerFactory = null
+    let loggerSpy = null
+
+    before('require the module', function () {
+      loggerSpy = sinon.spy(Logger)
+
+      ConsoleLoggerFactory = proxyquire('../src/console-logger-factory', {
+        './logger': loggerSpy
+      })
+    })
+
+    afterEach('reset the history', function () {
+      loggerSpy.resetHistory()
+    })
+
     it('should create Logger with stdout stream', async function () {
       const logger = await ConsoleLoggerFactory.create()
 
